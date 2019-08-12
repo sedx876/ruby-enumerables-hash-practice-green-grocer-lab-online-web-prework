0589fd0205eec07621b45796fe7cfced3153dbfe
Sharon Watkins <swatkins876@gmail.com>
2019-08-12 11:56:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,11 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # code here
+  {
+  "PEANUT BUTTER" => {:price => 2.40, :clearance => true,  :count => 2},
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK"     => {:price => 3.60, :clearance => true,  :count => 1}
+}
 end
 
 def checkout(cart, coupons)
